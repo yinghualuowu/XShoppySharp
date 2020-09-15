974e67ee9bb6c09b624c97a13e2bb7e8009fb06c
yinghualuowu <1048146365@qq.com>
2020-09-15 11:08:57 +0800

    更新md

--- XShoppySharp/Entities/XShoppyObject.cs
+++ XShoppySharp/Entities/XShoppyObject.cs
@@ -2,17 +2,33 @@
 
 namespace XShoppySharp.Entities
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class XShoppyObject<T>
     {
+        /// <summary>
+        /// 
+        /// </summary>
         [JsonProperty("code")]
         public int Code { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [JsonProperty("msg")]
         public string Msg { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [JsonProperty("now")]
         public string Now { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [JsonProperty("data")]
         public T Data { get; set; }
     }
